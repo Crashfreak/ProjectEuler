f83124bc572c68d7c0a0297b41d66ab1bbaa7178
cuthbertm <Timel@pse115>
2015-05-29 11:44:01 -0700

    Updating problem 15 to be a lot more efficient.

--- src/crashfreak/problems/Problem15.java
+++ src/crashfreak/problems/Problem15.java
@@ -2,11 +2,18 @@ package crashfreak.problems;
 
 import crashfreak.EulerProblem;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * This recursive function is correct, but gets super slow with larger grids
  * 20x20 takes 1846103ms (half an hour) = 137846528820
  * 15x15 takes 2490ms (2 seconds)
  * So it is O(n2)
+ *
+ * Cleaned up the code, but leaving the original comment so as to show the difference
+ * In the new version the 20x20, in now takes 6ms so big improvement by adding caching.
+ * This updated solution was taken from http://stackoverflow.com/questions/2200236/project-euler-15
  *
  * A mathematical solution, which will give the immediate answer is simply 40 choose 20
  *
@@ -14,8 +21,10 @@ import crashfreak.EulerProblem;
  */
 public class Problem15 extends EulerProblem {
 
-    public static int maxX = 15;
-    public static int maxY = 15;
+    public static int maxX = 20;
+    public static int maxY = 20;
+
+    static Map<String, Long> pathsByBlock = new Hashtable<String, Long>();
 
     class Point {
         public int x;
@@ -32,6 +41,8 @@ public class Problem15 extends EulerProblem {
         public Point moveDown() {
             return new Point(x, y+1);
         }
+
+        public long surfaceArea() { return (maxX - x) * (maxY - y); }
     }
 
     @Override
@@ -40,16 +51,22 @@ public class Problem15 extends EulerProblem {
     }
 
     public long traverse(Point point) {
-        if (point.x == maxX && point.y == maxY) {
+        if (point.surfaceArea() == 0) {
             return 1;
-        } else if (point.x == maxX) {
-            return traverse(point.moveDown());
-        } else if (point.y == maxY) {
-            return traverse(point.moveRight());
-        } else {
-            long right = traverse(point.moveRight());
-            long left = traverse(point.moveDown());
-            return right + left;
         }
+        long i = 0;
+
+        String block = (maxX - point.x) + "x" + (maxY - point.y) + "y";
+        if (!pathsByBlock.containsKey(block)) {
+            if (point.x < maxX) {
+                i += traverse(point.moveDown());
+            }
+            if (point.y < maxY) {
+                i += traverse(point.moveRight());
+            }
+            pathsByBlock.put(block, i);
+        }
+
+        return pathsByBlock.get(block);
     }
 }
